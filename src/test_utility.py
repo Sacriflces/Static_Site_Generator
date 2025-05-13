7884ff8d8ef3cb5bf2a9e9c3b8612c20a44f99cf
Sacriflces <Jmhservice@gmail.com>
2025-05-13 13:00:08 -0400

    Working on split_nodes_delimiter function

--- src/test_utility.py
+++ src/test_utility.py
@@ -1,11 +1,52 @@
 import unittest
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from utility import text_node_to_html_node
+from utility import text_node_to_html_node, split_nodes_delimiter
 
 class TestUtility(unittest.TestCase):
     def test_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
         sut = text_node_to_html_node(node)
-        self.assertEqual(sut.tag, None)
-        self.assertEqual(sut.value, "This is a text node")
+        expected_html = "This is a text node"
+        self.assertEqual(sut.to_html(), expected_html)
+    
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        sut = text_node_to_html_node(node)
+        expected_html = "<b>This is a bold node</b>"
+        self.assertEqual(sut.to_html(), expected_html)
+
+
+    def test_italic(self):
+        node = TextNode("This is an italic node", TextType.ITALIC)
+        sut = text_node_to_html_node(node)
+        expected_html = "<i>This is an italic node</i>"
+        self.assertEqual(sut.to_html(), expected_html)
+    
+    def test_code(self):
+        node = TextNode("This is a code node", TextType.CODE)
+        sut = text_node_to_html_node(node)
+        expected_html = "<code>This is a code node</code>"
+        self.assertEqual(sut.to_html(), expected_html)
+
+    def test_link(self):
+        node = TextNode("This is a link node", TextType.LINK, "https://example.com")
+        sut = text_node_to_html_node(node)
+        expected_html = '<a href="https://example.com">This is a link node</a>'
+        self.assertEqual(sut.to_html(), expected_html)
+    
+    def test_image(self):
+        node = TextNode("This is an image node", TextType.IMAGE, "https://example.com/image01")
+        sut = text_node_to_html_node(node)
+        expected_html = '<img "src=https://example.com/image01" alt="This is an image node"></img>'
+    
+    def test_split_node_single_nested_italic(self):
+        nodes = [TextNode("This is a text with a `code block` word")]
+        sut = split_nodes_delimiter(nodes, "`", TextType.Code)
+        self.assertEqual(sut, [TextNode("This is a text with a ", TextNode.TEXT),
+                               TextNode("code block", TextType.CODE),
+                               TextNode(" word", TextType.TEXT)])
+    
+
+if __name__ == "__main__":
+    unittest.main()
--- src/utility.py
+++ src/utility.py
@@ -18,4 +18,23 @@ def text_node_to_html_node(text_node):
             htmlNode = LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
 
     return htmlNode
-            
+
+def split_nodes_delimiter(old_nodes, delimiter, textType):
+    new_nodes = []
+    for node in old_nodes:
+        new_nodes_text = node.text.split(delimiter)
+        if (len(new_nodes_text) == 1):
+            new_nodes.append(node)
+            continue
+        for index in range(0, new_nodes_text):
+            if new_nodes_text == "":
+                continue
+            new_nodes.append(TextNode(new_nodes_text[index], textType if index % 2 == 1 else TextType.TEXT))
+    return new_nodes
+
+if __name__ == "__main__":
+    split_nodes_delimiter([TextNode("This is a text with a `code block` word", TextType.ITALIC)], "`", TextType.CODE)
+    split_nodes_delimiter(["This is a text with a `code block`"], "`", TextType.CODE)
+    split_nodes_delimiter(["This is a text with a without a code block word"], "`", TextType.CODE)
+    split_nodes_delimiter(["This is a text with a `code block` word and another `code block`"], "`", TextType.CODE)
+    split_nodes_delimiter(["This is a text with a `code block``code block`"], "`", TextType.CODE)
